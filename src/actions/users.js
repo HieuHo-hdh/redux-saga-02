5780b5d072a6d798f51573262125b1727c66d7e6
Hieu <hdh13300@gmail.com>
2020-12-21 22:46:58 +0700

    First Commit

---
+++ src/actions/users.js
@@ -0,0 +1,23 @@
+//Define action Types
+export const Types = {
+    GET_USERS_REQUEST: 'users/get_users_request',
+    GET_USERS_SUCCESS: 'users/get_users_success'
+};
+
+//Define actual actions
+//get user request([no para]), return type: GET_USERS_REQUEST
+export const getUsersRequest = () =>
+({
+    type: Types.GET_USERS_REQUEST
+});
+
+export const getUsersSuccess = ({items}) //items: array of users be passed. When call getUsersSuccess array of users (item) comes from user API
+//within saga, call getUsersSuccess, pass in User list from API to this {items} property
+=>
+({
+    type: Types.GET_USERS_SUCCESS,
+    payload:
+    {
+        items
+    }
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
+import Axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+//axios can call api to get data:
+//axios.get('http://rem-rest-api.herokuapp.com/api/users') //because of APi so link have to be linked with http://rem-rest-api.herokuapp.com/api
+import reducers from './reducers';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+axios.defaults.withCredentials=true;
+axios.defaults.baseURL='http://rem-rest-api.herokuapp.com/api';
+
+const store = createStore(reducers);
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store = {store}><App /></Provider>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.unregister();
